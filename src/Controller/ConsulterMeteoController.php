54fe62fb7ca4d4d8005c32754aafb57018e16a2a
siteFB <127745645+siteFB@users.noreply.github.com>
2023-06-05 10:23:54 +0200

    fixtures for ephemeris succeeded

--- src/Controller/ConsulterMeteoController.php
+++ src/Controller/ConsulterMeteoController.php
@@ -5,31 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EphemerideRepository;
 
 #[Route('/consulter/meteo', name: 'app_consulter_meteo_')]
 class ConsulterMeteoController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(EphemerideRepository $ephemerideRepository): Response
     {
-        return $this->render('consulter_meteo/index.html.twig');
-    }
-
-    #[Route('/today', name: 'jour')]
-    public function today(): Response
-    {
-        return $this->render('consulter_meteo/jour.html.twig');
-    }
-
-    #[Route('/week', name: 'semaine')]
-    public function week(): Response
-    {
-        return $this->render('consulter_meteo/semaine.html.twig');
-    }
-
-    #[Route('/fifteen', name: 'quinzaine')]
-    public function fifteen(): Response
-    {
-        return $this->render('consulter_meteo/quinzaine.html.twig');
+        return $this->render('consulter_meteo/index.html.twig', [
+            'ephemeride' => $ephemerideRepository->findBy([])
+        ]);
     }
 }
